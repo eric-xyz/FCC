f6335df968cd2d249bbf54de29969bec7cefe8f7
eric0609 <yaozhang.xu@gmail.com>
2016-10-03 20:39:40 -0500

    add animation on input

--- wikiReader/wikiReader.js
+++ wikiReader/wikiReader.js
@@ -1,8 +1,22 @@
-var app = angular.module('firstApp',[]);
+var firstApp = angular.module('firstApp',['ngAnimate']);
 
-app.controller('ctrl', function($scope, $http){
-	$scope.getForm = function(){
-		
+firstApp.controller('ctrl', function($scope, $http){
+	$scope.showIcon = true;
+	$scope.inputClass = "";
+	$scope.inputHide = true;
+	$scope.keyword ="";
+	//var textInput = $("#keyWordInput");
+
+	$scope.hideIcon = function(){
+		$scope.showIcon = false;
+		$scope.inputClass = "keyWordInput";
+		$scope.inputHide = false;
+		setTimeout(function(){
+            $("#keyWordInput").focus();
+        }, 1);
 	}
-
-})
+	$scope.fetchData = function(){
+		console.log($scope.keyword);
+	}
+	//$scope.iconDivClass = iconClass;
+});
