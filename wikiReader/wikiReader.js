8a8bb8f54607c49cfd85cbea67003b3b3ca8f6df
eric0609 <yaozhang.xu@gmail.com>
2016-10-03 22:38:27 -0500

    finish parse query

--- wikiReader/wikiReader.js
+++ wikiReader/wikiReader.js
@@ -1,11 +1,16 @@
 var firstApp = angular.module('firstApp',['ngAnimate']);
 
-firstApp.controller('ctrl', function($scope, $http){
+firstApp.controller('ctrl', function($scope, $http, $log){
 	$scope.showIcon = true;
 	$scope.inputClass = "";
 	$scope.inputHide = true;
 	$scope.keyword ="";
-	//var textInput = $("#keyWordInput");
+	// var baseUrl = "https://en.wikipedia.org/w/api.php?"
+	// 					+"action=query&format=json&list=search&srsearch=";
+	var baseUrl ="https://en.wikipedia.org/w/api.php?format=json&action=query&" + 
+				 "generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts" + 
+				 "&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+    var suffix = "&callback=JSON_CALLBACK";
 
 	$scope.hideIcon = function(){
 		$scope.showIcon = false;
@@ -16,7 +21,23 @@ firstApp.controller('ctrl', function($scope, $http){
         }, 1);
 	}
 	$scope.fetchData = function(){
-		console.log($scope.keyword);
+		if(typeof $scope.keyword != undefined && 
+			$scope.keyword != ""){
+			getWikiResult();
+		}
+		
 	}
+	var getWikiResult = function(){
+		var realUrl = baseUrl+$scope.keyword+suffix;
+		$http.jsonp(realUrl).then(function(response){
+			//$log.info(response);
+			angular.forEach(response.data.query.pages, function(v , k){
+				console.log(v.title);
+			});
+		}, function(response){
+			console.log("fail");
+		});
+	}
+
 	//$scope.iconDivClass = iconClass;
 });
